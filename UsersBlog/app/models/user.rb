c5500871c581beaca626b685e7901a02c857213d
Andrew Magdy <andrewtawf@gmail.com>
2015-02-21 14:06:43 +0200

    Remove

--- UsersBlog/app/models/user.rb
+++
@@ -1,7 +0,0 @@
-class User < ActiveRecord::Base
-	validates :email, presence: true, uniqueness: true
-	validates :password, presence: true
-	has_many :posts
-	has_many :comments
-	has_many :replies
-end
--- UsersBlog/app/views/users/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @user, :id, :email, :password, :username, :photo, :admin, :signature, :created_at, :updated_at
