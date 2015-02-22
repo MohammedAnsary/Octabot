d8ccba7c147e7c8a273ddfd7207ba7b359637bae
MohammedAnsary <a.n.s.a.r.y@hotmail.com>
2015-02-23 00:40:47 +0200

    Rebasing and BlogAdding

--- Blog/app/views/posts/index.json.jbuilder
+++
@@ -1,4 +0,0 @@
-json.array!(@posts) do |post|
-  json.extract! post, :id, :category, :title, :content, :timing
-  json.url post_url(post, format: :json)
-end
--- Blog/db/migrate/20150221185341_create_high_scores.rb
+++
@@ -1,9 +0,0 @@
-class CreateHighScores < ActiveRecord::Migration
-  def change
-    create_table :high_scores do |t|
-      t.text :content
-
-      t.timestamps null: false
-    end
-  end
-end
