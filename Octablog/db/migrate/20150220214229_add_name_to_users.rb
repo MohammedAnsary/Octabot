caa2fb9787a59034632358ce656ec0e3ecff7384
mayarali <mayar.bassel.ali@gmail.com>
2015-02-27 16:52:16 +0200

    Octablog correction

--- Octablog/db/migrate/20150220214229_add_name_to_users.rb
+++
@@ -1,7 +0,0 @@
-class AddNameToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :name, :string
-    add_column :users, :signature, :string
-    add_column :users, :is_admin, :boolean, default: false
-  end
-end
--- Octablog/db/migrate/20150221202230_add_post_to_comment.rb
+++
@@ -1,4 +0,0 @@
-class AddPostToComment < ActiveRecord::Migration
-  def change
-  end
-end
